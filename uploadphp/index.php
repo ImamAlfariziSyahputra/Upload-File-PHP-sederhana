8e94b02b1ec38c5420610e5729a6fed762ac5fdd
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-06-15 14:49:13 +0700

    Membuat Upload File pada PHP
    Still Error at : move_uploaded_file Line 17

---
+++ uploadphp/index.php
@@ -0,0 +1,29 @@
+<?php 
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP File Upload</title>
+</head>
+<body>
+    <?php 
+        if (isset($_SESSION['message']) && $_SESSION['message']) {
+            printf('<b>%s</b>', $_SESSION['message']);
+            unset($_SESSION['message']);
+        }
+    ?>
+
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <div>
+            <span>Upload File:</span>
+            <input type="file" name="uploadedFile">
+        </div>
+
+        <input type="submit" name="uploadBtn" value="Upload">
+    </form>
+</body>
+</html>
---
+++ uploadphp/upload.php
@@ -0,0 +1,25 @@
+<?php 
+
+if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
+    $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+    $fileName = $_FILES['uploadedFile']['name'];
+    $fileSize = $_FILES['uploadedFile']['size'];
+    $fileType = $_FILES['uploadedFile']['type'];
+    $fileNameCmps = explode(".", $fileName);
+    $fileExtension = strtolower(end($fileNameCmps));
+    $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+
+    $allowedFileExtension = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
+    if (in_array($fileExtension, $allowedFileExtension)) {
+        $uploadFileDir = './uploaded_files/';
+        $dest_path = $uploadFileDir . $newFileName;
+
+        if (move_uploaded_file($fileTmpPath, $dest_path)) {
+            $message = 'File Succesfully Upload.';            
+        } else {
+            $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+        }
+    }
+}
+
+?>
