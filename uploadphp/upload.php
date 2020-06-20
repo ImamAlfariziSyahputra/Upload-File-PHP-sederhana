86d8648567733fc005c536044c83d51a4f77e6f6
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-06-20 14:00:15 +0700

    Menambah Fitur memasukkan Data ke dalam Database
    1. Menambah Fitur memasukkan nama File ke dalam Database
    2. Merubah Struktur Coding dengan cara saya sendiri
    3. Menambahkan Script Alert jika data berhasil atau gagal ditambah

--- uploadphp/upload.php
+++ uploadphp/upload.php
@@ -1,26 +1,74 @@
 <?php 
 
-if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
-    $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
-    $fileName = $_FILES['uploadedFile']['name'];
-    $fileSize = $_FILES['uploadedFile']['size'];
-    $fileType = $_FILES['uploadedFile']['type'];
-    $fileNameCmps = explode(".", $fileName);
-    $fileExtension = strtolower(end($fileNameCmps));
-    $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+$conn = mysqli_connect("localhost", "root", "", "db_upload");
 
-    $allowedFileExtension = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
-    if (in_array($fileExtension, $allowedFileExtension)) {
-        $uploadFileDir = './uploaded_files/';
-        $dest_path = $uploadFileDir . $newFileName;
+function tambah($data) {
+    global $conn;
+    
+    $namaFile = upload();
+    if (!$namaFile) {
+        return false;
+    }
 
-        if (move_uploaded_file($fileTmpPath, $dest_path)) {
-            $message = 'File Succesfully Upload.';
-            echo "Menuju File : <a href='".$uploadFileDir.$newFileName."'>".$newFileName."</a>";
+    $query = "INSERT INTO `file` (`id`, `nama_file`)
+            VALUES ('', '$namaFile')";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function upload() {
+    $namaFile = $_FILES['uploadedFile']['name'];
+    $ukuranFile = $_FILES['uploadedFile']['size'];
+    $error = $_FILES['uploadedFile']['error'];
+    $tmpName = $_FILES['uploadedFile']['tmp_name'];
+
+    if ($error == 4) {
+        echo "
+            <script>
+                alert('Upload File terlebih dahulu!');               
+            </script>
+            ";
+        return false;            
+    }
+
+    $ekstensiFileValid = ['jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc'];
+    $ekstensiFile = explode(".", $namaFile);
+    $ekstensiFile = strtolower(end($ekstensiFile));
+    if( !in_array($ekstensiFile, $ekstensiFileValid)) {
+        echo "
+            <script>
+                alert('Ekstensi File Dilarang!');               
+            </script>
+            ";
+    }
+
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $ekstensiFile;
+    
+    move_uploaded_file($tmpName, 'img/'.$namaFileBaru);
+
+    return $namaFileBaru;
+}
+
+    if (isset($_POST["uploadBtn"])) {
+        if ( tambah($_POST) > 0) {
+            echo "
+                <script>
+                    alert('File Berhasil Ditambah!');               
+                </script>
+                ";
+            echo "File Succesfully Uploaded!";
         } else {
-            $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+            echo "
+                <script>
+                    alert('File Gagal Ditambah!');
+                    document.location.href = 'index.php';
+                </script>
+                ";
         }
     }
-}
 
 ?>
